ce08309b823f699f435df8c134ca755c8cf1553f
Abhishek2kr <abhishek2krs@gmail.com>
2022-09-07 20:28:58 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,37 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TreeviewItem } from 'ngx-treeview';
-import {AppService} from './app.service'
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ],
-  providers: [
-        AppService
-    ]
+  template: `<ngx-treeview [items]="items"></ngx-treeview>`
 })
-
-
 export class AppComponent implements OnInit {
-    items: TreeviewItem[];
+  items: TreeviewItem[];
+  ngOnInit() {
+    this.items = this.getItems();
+  }
 
-    constructor() { }
+  getItems() {
+    // fetch api response
+    // convert response into this format (object can be nested, should contain below keys only with given type)
+    // {
+    //     text: string;
+    //     value: any;
+    //     disabled ?: boolean;
+    //     checked ?: boolean;
+    //     collapsed ?: boolean;
+    //     children ?: TreeItem[];
+    // }
 
-    ngOnInit() {
-        this.items = this.getItems();
-    }
-
-    getItems(){
-        // fetch api response
-        // convert response into this format (object can be nested, should contain below keys only with given type)    
-        // {
-        //     text: string;
-        //     value: any;
-        //     disabled ?: boolean; 
-        //     checked ?: boolean;
-        //     collapsed ?: boolean;
-        //     children ?: TreeItem[];
-        // }
-        
-        const item = new TreeviewItem({
-            text: 'Children', value: 1, children: [
-                { text: 'Baby 3-5', value: 11 },
-                { text: 'Baby 6-8', value: 12 },
-                { text: 'Baby 9-12', value: 13 }
-            ]
-        });
-        return [ item ];
-    }
+    const item = new TreeviewItem({
+      text: 'Children',
+      value: 1,
+      children: [
+        { text: 'Baby 3-5', value: 11 },
+        { text: 'Baby 6-8', value: 12 },
+        { text: 'Baby 9-12', value: 13 },
+      ],
+    });
+    return [item];
+  }
 }
